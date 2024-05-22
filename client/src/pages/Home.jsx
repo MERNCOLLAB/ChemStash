f147ce485124459eb857689c4d92e30c5ddf5510
bryancastillo10 <bryanangelocastillo.10@gmail.com>
2024-05-22 21:35:12 +0800

    added new button with STYLE

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
       <p>try 2</p>
       <p>another p</p>
       <p>try 3 with slack </p>
-      <button>New Button</button>
+      <button className="bg-blue-500 rounded-xl text-white">New Button</button>
     </div>
   );
 }
