be5300b6f51339ecbcefcab282a7b7dba3c277ac
bryancastillo10 <bryanangelocastillo.10@gmail.com>
2024-05-23 20:32:50 +0800

    fixed some typo errors

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -2,10 +2,13 @@ import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
     const dispatch = useDispatch();
-    const handleGoogleClick = async() => {
+    const navigate = useNavigate();
+
+    const handleGoogleClick = async () => {
         try {
             const provider = new GoogleAuthProvider();
             const auth = getAuth(app);
@@ -23,15 +26,17 @@ export default function OAuth() {
             });
             const data = await res.json();
             dispatch(signInSuccess(data));
+
+            navigate('/');
         }
-        catch(error) {
+        catch (error) {
             console.log("Could not login with Google", error)
         }
-  }  
-  return (
-      <button type="button" onClick={handleGoogleClick}
-          className='bg-green-700 text-white border-[1px] border-white p-3 uppercase hover:opacity-95'>
-          Continue with Google
-      </button>
-  )
-};
+    }
+    return (
+        <button type="button" onClick={handleGoogleClick}
+            className='bg-green-700 text-white border-[1px] border-white p-3 uppercase hover:opacity-95'>
+            Continue with Google
+        </button>
+    )
+}
