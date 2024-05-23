99cf946428b3433d6176011569b0c5203d750d7d
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-05-23 16:59:25 +0800

    create and add OAuth component

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { signInStart, signInFailure, signInSuccess } from "../redux/user/userSlice";
 import { useDispatch, useSelector } from "react-redux";
+import OAuth from "../components/OAuth";
 
 function SignIn() {
   const [formData, setFormData] = useState({});
@@ -64,6 +65,7 @@ function SignIn() {
         >
           {loading ? "Loading" : "Sign In"}
         </button>
+        <OAuth/>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Don&#39;t have an account?</p>
