0ea2e90ea8756a23ff85491b3f25d72be85107a1
Kevin Divinagracia <74808131+ghekghek2@users.noreply.github.com>
2024-05-24 14:16:56 +0800

    update headear component
    change secondary button color
    create private route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,30 @@
 import { Link } from "react-router-dom";
-
+import { useSelector } from "react-redux";
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="mx-auto border">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold">Auth App</h1>
         </Link>
-        <ul className="flex gap-4">
+        <ul className="flex gap-4 items-center">
           <Link to="/">
             <li>Home</li>
           </Link>
           <Link to="/about">
             <li>About</li>
           </Link>{" "}
-          <Link to="/sign-in">
-            <li>Sign In</li>
+          <Link to={currentUser ? "/profile" : "/sign-in"}>
+            {currentUser ? (
+              <img
+                className="border rounded-full h-10 w-10 object-cover"
+                src={currentUser.profilePicture}
+                alt=""
+              />
+            ) : (
+              <li>Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
