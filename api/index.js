7c51f096f9c42a99c1ed948d958430544e62105e
Kevin Divinagracia <74808131+ghekghek2@users.noreply.github.com>
2024-05-26 00:54:40 +0800

    change res to req

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ const __dirname = path.resolve();
 const app = express();
 app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("*", (req, res) => {
-  req.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 app.use(express.json());
 app.use(cookieParser());
