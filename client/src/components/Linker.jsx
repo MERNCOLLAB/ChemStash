ace0e67e80d08229571ff39740a11ebb03479043
bryancastillo10 <bryanangelocastillo.10@gmail.com>
2024-05-28 00:23:16 +0800

    create a reuseable component for Link

---
+++ client/src/components/Linker.jsx
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom";
+
+function Linker({ to, children }) {
+
+    return (
+        <NavLink
+            className={({ isActive }) => ` ${isActive ? 'active' : ''}`}
+            to={to}>
+            {children}
+        </NavLink>
+    )
+}
+
+export { Linker };
+
