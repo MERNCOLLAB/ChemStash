479d3aa5c38771ed791e0c70bc140576c9035abe
Kevin Divinagracia <74808131+ghekghek2@users.noreply.github.com>
2024-05-28 16:15:42 +0800

    fix input style

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,15 +1,12 @@
-
-
-export function Input({ 
-    type, placeholder, id, onChange, defaultValue}) {
+export function Input({ type, placeholder, id, onChange, defaultValue }) {
   return (
-      <input
-          id={id}
-          type={type}
-          placeholder={placeholder}
-          defaultValue={defaultValue}
-          onChange={onChange}
-          className="bg-slate-900 p-2"
-      />
-  )
+    <input
+      id={id}
+      type={type}
+      placeholder={placeholder}
+      defaultValue={defaultValue}
+      onChange={onChange}
+      className="bg-slate-900 p-3 border"
+    />
+  );
 }
