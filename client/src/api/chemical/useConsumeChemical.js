ffa0d44cf7e260af87fcc2f28dabb0c6326a2c53
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-07-16 13:46:33 +0800

    change chem consumption to work with backend

--- client/src/api/chemical/useConsumeChemical.js
+++ client/src/api/chemical/useConsumeChemical.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
-
+import toast from 'react-hot-toast';
 const useConsumeChemical = () => {
   const [loading, setLoading] = useState(false);
   const consumeChemical = async (update, getChemicalList) => {
     const { id, amount, unit, user } = update;
+
     try {
       setLoading(true);
       const response = await fetch(`/api/chemical/${id}/consume`, {
@@ -11,12 +12,14 @@ const useConsumeChemical = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ amount, unit, user }),
+        body: JSON.stringify({ id, amount, unit, user }),
       });
 
       if (!response.ok) {
+        toast.error('Failed to update chemical amount');
         throw new Error('Network response was not ok');
       }
+      toast.success('Chemical amount reduced');
       getChemicalList();
     } catch (error) {
       console.error('Error consuming chemical:', error);
