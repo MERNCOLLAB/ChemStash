1eac6966b2845f9e2f306afa523bb787ff251a3c
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-07-21 13:05:08 +0800

    Button at HomePage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,37 @@
-function Home() {
-  return (
-    <div>
-      Home
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../components';
+import { useSelector } from 'react-redux';
 
+const Home = () => {
+  const navigate = useNavigate();
+
+  const { currentUser } = useSelector((state) => state.user);
+  const isAuthenticated = currentUser !== null;
+  const role = currentUser?.role;
+
+  const handleGetStarted = () => {
+    if (isAuthenticated && role) {
+      navigate(`/${role}/dashboard`);
+    } else {
+      navigate('/sign-in');
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <div className="">
+        <h1 className="font-semibold">Welcome to ChemStack!</h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mt-4">
+        <Button onClick={handleGetStarted} type="button" variant="primary">
+          Get Started
+        </Button>
+        <Button onClick={() => navigate('/about')} type="button" variant="secondary">
+          About
+        </Button>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
