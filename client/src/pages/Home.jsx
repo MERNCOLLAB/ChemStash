8986eb65a6ce1d3fcb97e2d089a62961a987db9e
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-07-30 21:52:29 +0800

    add style to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useNavigate } from 'react-router-dom';
 import { Button } from '../components';
 import { useSelector } from 'react-redux';
+import HeroImg from '../assets/lab-home.png';
+import HeroBg from '../assets/hexagon-bg.svg';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -17,21 +19,37 @@ const Home = () => {
     }
   };
 
+  const HeroBackground = {
+    backgroundImage: `url(${HeroBg})`,
+    backgroundPosition: 'center',
+    backgroundSize:'cover',
+    backgroundRepeat:'no-repeat'
+  }
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <div className="">
-        <h1 className="font-semibold">Welcome to ChemStack!</h1>
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 items-center min-h-screen" style={HeroBackground}>
+      <div className="p-8 grid grid-cols-1 justify-center items-center">
+        <h1 className='text-2xl font-semibold tracking-wider text-center md:text-left'>
+          Revolutionize your chemical inventory management with <span className='text-indigo1'>ChemStack!</span>
+        </h1>
+        <p className='mt-4'> Streamline your chemical inventory management process with ease. 
+          Bringing <span className='text-indigo0 font-semibold'>efficiency</span> and <span className='font-semibold text-indigo0'> quality assurance</span> to laboratory management </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mt-8 max-w-[70%] mx-auto md:mx-0">
+          <Button onClick={handleGetStarted} type="button" variant="primary">
+            Get Started
+          </Button>
+          <Button onClick={() => navigate('/about')} type="button" variant="secondary">
+            About
+          </Button>
+        </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mt-4">
-        <Button onClick={handleGetStarted} type="button" variant="primary">
-          Get Started
-        </Button>
-        <Button onClick={() => navigate('/about')} type="button" variant="secondary">
-          About
-        </Button>
+      <div className="flex flex-col md:flex-row items-center">
+      <img src={HeroImg} className='size-3/4' alt="hero-image"/>
       </div>
+      
     </div>
   );
 };
 
 export default Home;
+
+
