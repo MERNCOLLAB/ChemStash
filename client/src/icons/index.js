41a663c214affd7994fa8b31a562dea09ee55860
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-08-01 11:59:13 +0800

    reviseDashboardIcon

--- client/src/icons/index.js
+++ client/src/icons/index.js
@@ -2,5 +2,6 @@ import DropIcon from './DropIcon';
 import OutOfStockIcon from './OutOfStockIcon';
 import FlaskIcon from './FlaskIcon';
 import ExpiredIcon from './ExpiredIcon';
+import ExpiredChemical from './ExpiredChemical';
 
-export { DropIcon, OutOfStockIcon, FlaskIcon, ExpiredIcon };
+export { DropIcon, OutOfStockIcon, FlaskIcon, ExpiredIcon,  ExpiredChemical };
