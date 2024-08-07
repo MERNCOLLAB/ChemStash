fe86115e593f2354191610b824c83a84fdd5d141
Kevin Divinagracia <74808131+ghekghek2@users.noreply.github.com>
2024-08-07 13:32:43 +0800

    improve marquee

--- client/src/pages/landing-page/Hero.jsx
+++ client/src/pages/landing-page/Hero.jsx
@@ -1,132 +1,135 @@
-import bg1 from '../../assets/bg1.png';
+import tests from '../../assets/tests.png';
 import { Button } from '../../components';
 
-const Hero = ({handleGetStarted}) => {
+const Hero = ({ handleGetStarted }) => {
   return (
     <>
       <div className=" flex flex-col justify-center h-full w-full bg-white1 space-y-4 md:order-2 order-1">
-            <h1 className="text-6xl font-bold">Revolutionize your chemical inventory management with Chemstack!</h1>
-            <p>
-                Streamline your chemical inventory management process with ease. Bringing efficiency and quality assurance
-                to laboratory management
-            </p>
-            <div className="my-4">
-                <Button onClick={handleGetStarted} type="button" variant="primary">
-                Get Started
-                </Button>
-            </div>
-            </div>
-        
-        <div className="w-full h-full  md:order-3 order-2 relative">
+        <h1 className="text-6xl font-bold">Revolutionize your chemical inventory management with Chemstack!</h1>
+        <p>
+          Streamline your chemical inventory management process with ease. Bringing efficiency and quality assurance to
+          laboratory management
+        </p>
+        <div className="my-4">
+          <Button onClick={handleGetStarted} type="button" variant="primary">
+            Get Started
+          </Button>
+        </div>
+      </div>
+
+      <div className="w-full h-full  md:order-3 order-2 relative">
         {/* SVG Layer */}
-        <svg className="w-fit h-fit" viewBox="0 0 624 662" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g filter="url(#filter0_i_680_1487)">
+        <svg className="w-full h-full" viewBox="0 0 637 662" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g filter="url(#filter0_i_711_1498)">
             <path
-            d="M10.5 473.5C10.5 454.998 25.4985 440 44 440V440C62.5015 440 77.5 454.998 77.5 473.5V615.5C77.5 634.002 62.5015 649 44 649V649C25.4985 649 10.5 634.002 10.5 615.5V473.5Z"
-            fill="url(#paint0_linear_680_1487)"
-            fillOpacity="0.5"
+              d="M10.2227 474.253C10.2227 455.336 25.5582 440 44.4755 440V440C63.3928 440 78.7283 455.336 78.7283 474.253V614.747C78.7283 633.664 63.3928 649 44.4755 649V649C25.5582 649 10.2227 633.664 10.2227 614.747V474.253Z"
+              fill="url(#paint0_linear_711_1498)"
+              fill-opacity="0.5"
             />
-        </g>
-        <g filter="url(#filter1_i_680_1487)">
+          </g>
+          <g filter="url(#filter1_i_711_1498)">
             <path
-            d="M11.5 52C11.5 29.9086 29.4086 12 51.5 12H573.5C595.591 12 613.5 29.9086 613.5 52V612C613.5 634.091 595.591 652 573.5 652H127C104.909 652 87 634.091 87 612V470.95C87 450.101 70.0987 433.2 49.25 433.2V433.2C28.4013 433.2 11.5 416.299 11.5 395.45V52Z"
-            fill="url(#paint1_linear_680_1487)"
-            fillOpacity="0.5"
+              d="M10.2461 52C10.2461 29.9086 28.1547 12 50.2461 12H585.77C607.862 12 625.77 29.9086 625.77 52V612C625.77 634.091 607.862 652 585.77 652H127.442C105.351 652 87.4422 634.091 87.4422 612V471.798C87.4422 450.481 70.1613 433.2 48.8442 433.2V433.2C27.527 433.2 10.2461 415.919 10.2461 394.602V52Z"
+              fill="url(#paint1_linear_711_1498)"
+              fill-opacity="0.5"
             />
-        </g>
-        <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M0.5 0H623.5V662H0.5V0ZM10.5 473.5C10.5 454.998 25.4985 440 44 440C62.5015 440 77.5 454.998 77.5 473.5V611.5C77.5 630.002 62.5015 645 44 645C25.4985 645 10.5 630.002 10.5 611.5V473.5ZM10.5 52C10.5 29.9086 28.4086 12 50.5 12H572.5C594.591 12 612.5 29.9086 612.5 52V612C612.5 634.091 594.591 652 572.5 652H126C103.909 652 86 634.091 86 612V470.95C86 450.101 69.0987 433.2 48.25 433.2C27.4013 433.2 10.5 416.299 10.5 395.45V52Z"
+          </g>
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M0 0H636.996V662H0V0ZM10.2227 474.253C10.2227 455.336 25.5583 440 44.4756 440C63.3929 440 78.7284 455.336 78.7284 474.253V610.747C78.7284 629.664 63.3929 645 44.4756 645C25.5583 645 10.2227 629.664 10.2227 610.747V474.253ZM10.2189 52C10.2189 29.9086 28.1275 12 50.2189 12H585.743C607.835 12 625.743 29.9086 625.743 52V612C625.743 634.091 607.835 652 585.743 652H127.415C105.324 652 87.4151 634.091 87.4151 612V471.798C87.4151 450.481 70.1341 433.2 48.817 433.2C27.4999 433.2 10.2189 415.919 10.2189 394.602V52Z"
             fill="#F2F5F9"
-        />
-        <defs>
+          />
+          <defs>
             <filter
-            id="filter0_i_680_1487"
-            x="10.5"
-            y="440"
-            width="69"
-            height="217"
-            filterUnits="userSpaceOnUse"
-            colorInterpolationFilters="sRGB"
+              id="filter0_i_711_1498"
+              x="10.2227"
+              y="440"
+              width="70.5039"
+              height="217"
+              filterUnits="userSpaceOnUse"
+              color-interpolation-filters="sRGB"
             >
-            <feFlood floodOpacity="0" result="BackgroundImageFix" />
-            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-            <feColorMatrix
+              <feFlood flood-opacity="0" result="BackgroundImageFix" />
+              <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+              <feColorMatrix
                 in="SourceAlpha"
                 type="matrix"
                 values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
                 result="hardAlpha"
-            />
-            <feOffset dx="2" dy="8" />
-            <feGaussianBlur stdDeviation="10" />
-            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
-            <feColorMatrix type="matrix" values="0 0 0 0 0.309804 0 0 0 0 0.329412 0 0 0 0 0.329412 0 0 0 0.5 0" />
-            <feBlend mode="normal" in2="shape" result="effect1_innerShadow_680_1487" />
+              />
+              <feOffset dx="2" dy="8" />
+              <feGaussianBlur stdDeviation="10" />
+              <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+              <feColorMatrix type="matrix" values="0 0 0 0 0.309804 0 0 0 0 0.329412 0 0 0 0 0.329412 0 0 0 0.5 0" />
+              <feBlend mode="normal" in2="shape" result="effect1_innerShadow_711_1498" />
             </filter>
             <filter
-            id="filter1_i_680_1487"
-            x="11.5"
-            y="12"
-            width="604"
-            height="648"
-            filterUnits="userSpaceOnUse"
-            colorInterpolationFilters="sRGB"
+              id="filter1_i_711_1498"
+              x="10.2461"
+              y="12"
+              width="617.523"
+              height="648"
+              filterUnits="userSpaceOnUse"
+              color-interpolation-filters="sRGB"
             >
-            <feFlood floodOpacity="0" result="BackgroundImageFix" />
-            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-            <feColorMatrix
+              <feFlood flood-opacity="0" result="BackgroundImageFix" />
+              <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+              <feColorMatrix
                 in="SourceAlpha"
                 type="matrix"
                 values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
                 result="hardAlpha"
-            />
-            <feOffset dx="2" dy="8" />
-            <feGaussianBlur stdDeviation="10" />
-            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
-            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0" />
-            <feBlend mode="normal" in2="shape" result="effect1_innerShadow_680_1487" />
+              />
+              <feOffset dx="2" dy="8" />
+              <feGaussianBlur stdDeviation="10" />
+              <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+              <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0" />
+              <feBlend mode="normal" in2="shape" result="effect1_innerShadow_711_1498" />
             </filter>
             <linearGradient
-            id="paint0_linear_680_1487"
-            x1="-77.5"
-            y1="412"
-            x2="236.5"
-            y2="739"
-            gradientUnits="userSpaceOnUse"
+              id="paint0_linear_711_1498"
+              x1="-79.7549"
+              y1="412"
+              x2="233.885"
+              y2="745.965"
+              gradientUnits="userSpaceOnUse"
             >
-            <stop stopColor="#00D9FF" stopOpacity="0.2" />
-            <stop offset="1" stopColor="#03A8C6" />
+              <stop stop-color="#00D9FF" stop-opacity="0.2" />
+              <stop offset="1" stop-color="#03A8C6" />
             </linearGradient>
             <linearGradient
-            id="paint1_linear_680_1487"
-            x1="-19"
-            y1="12"
-            x2="701"
-            y2="845.5"
-            gradientUnits="userSpaceOnUse"
+              id="paint1_linear_711_1498"
+              x1="-20.9391"
+              y1="12"
+              x2="696.567"
+              y2="861.273"
+              gradientUnits="userSpaceOnUse"
             >
-            <stop stopColor="#00D9FF" stopOpacity="0.5" />
-            <stop offset="1" stopColor="#03A8C6" />
+              <stop stop-color="#00D9FF" stop-opacity="0.5" />
+              <stop offset="1" stop-color="#03A8C6" />
             </linearGradient>
-        </defs>
+          </defs>
         </svg>
 
         {/* Background Image */}
-        <div className="flex flex-col  -z-20 absolute   w-fit h-fit top-0 p-2 ">
-        <div className="relative flex  overflow-x-hidden -z-20">
+        <div
+          className="flex flex-col  -z-20 absolute  top-0
+         p-2 w-[1410px] h-[646px]"
+        >
+          <div className="relative flex  overflow-x-hidden -z-20">
             <div className="animate-marquee">
-            <img src={bg1} alt="hero-bg" />
+              <img src={tests} alt="hero-bg" className="" />
             </div>
 
             <div className="animate-marquee2 absolute top-0">
-            <img src={bg1} alt="hero-bg" />
+              <img src={tests} alt="hero-bg" className="" />
             </div>
+          </div>
         </div>
-        </div>
-    </div>
-  </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
 export default Hero;
