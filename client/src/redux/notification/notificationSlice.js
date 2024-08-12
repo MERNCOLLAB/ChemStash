b3c591e6af1ca32bec584371c1816145237d956b
Kevin Divinagracia <74808131+ghekghek2@users.noreply.github.com>
2024-08-12 22:19:37 +0800

    test prod

--- client/src/redux/notification/notificationSlice.js
+++ client/src/redux/notification/notificationSlice.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 
 const initialState = {
   user: '',
-  socket: io('http://localhost:3000'),
+  socket: io('https://mern-with-auth-rba5.onrender.com/'),
 };
 
 const notificationSlice = createSlice({
