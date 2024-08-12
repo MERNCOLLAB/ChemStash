c4d468f0bf9c37156fc6a20f12879cbf7f54414d
Kevin Divinagracia <74808131+ghekghek2@users.noreply.github.com>
2024-08-12 22:39:42 +0800

    Revert "test prod"
    This reverts commit b3c591e6af1ca32bec584371c1816145237d956b.

--- client/src/redux/notification/notificationSlice.js
+++ client/src/redux/notification/notificationSlice.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 
 const initialState = {
   user: '',
-  socket: io('https://mern-with-auth-rba5.onrender.com/'),
+  socket: io('http://localhost:3000'),
 };
 
 const notificationSlice = createSlice({
